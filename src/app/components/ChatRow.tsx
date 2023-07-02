4952fab82c44d4b0c2464ec9b34145e21cef4526
Apple <farhad9>
2023-07-03 01:11:01 +0500

    remove chat

--- src/app/components/ChatRow.tsx
+++ src/app/components/ChatRow.tsx
@@ -1,5 +1,5 @@
 import { ChatBubbleLeftIcon, TrashIcon } from '@heroicons/react/24/outline'
-import { collection, orderBy, query } from 'firebase/firestore'
+import { collection, deleteDoc, doc, orderBy, query } from 'firebase/firestore'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
@@ -19,6 +19,10 @@ function ChatRow({id }: Props) {
         collection(db,"users", session?.user?.email!,'chats',id,'messages'),
         orderBy('createdAt','asc')
     ))
+    const deleteChat = async () => {
+        await deleteDoc(doc(db, "users",session?.user?.email!,'chats',id))
+        router.replace('/')
+    }
     useEffect(() => {
         if(!pathName) return;
         setActive(pathName.includes(id))
@@ -28,7 +32,7 @@ function ChatRow({id }: Props) {
     <Link href={`/chat/${id}`} className={`newChat-row justify-center ${active && 'bg-gray-700/50'}`}>
         <ChatBubbleLeftIcon className='h-5 w-5' />
         <p className='flex-1 hideen md:inline-flex truncate'>{messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}</p>
-        <TrashIcon className='h-5 w-5 text-gray-700 hover:text-red-700' />
+        <TrashIcon className='h-5 w-5 text-gray-700 hover:text-red-700' onClick={deleteChat} />
     </Link>
   )
 }
