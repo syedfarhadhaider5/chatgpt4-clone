12c6774ae4c5d1a22eecb698c626d311121b9c0c
Apple <farhad9>
2023-07-02 20:25:55 +0500

    add new chat in firebase db

--- src/app/components/NewChat.tsx
+++ src/app/components/NewChat.tsx
@@ -1,8 +1,28 @@
+"use client"
 import { PlusIcon } from "@heroicons/react/24/solid"
+import { db } from "./firebase";
+import { useRouter } from "next/navigation";
+import { getServerSession } from 'next-auth';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { useSession, signOut } from "next-auth/react"
 
 function NewChat() {
+
+  const router = useRouter();
+  const {data: session} = useSession();
+
+  const createNewChat = async ()  => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"),{
+          userId: session?.user?.email!,
+          createAt: serverTimestamp()
+      }
+    );
+    router.push(`/chat/${doc.id}`)
+  }
+
   return (
-    <div className="border border-gray-700 newChat-row">
+    <div onClick={createNewChat} className="border border-gray-700 newChat-row">
         <PlusIcon  className="h-4 w-4"/>
         <p>NewChat</p>
     </div>
