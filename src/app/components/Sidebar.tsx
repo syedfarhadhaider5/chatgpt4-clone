7724b4fb58ec185482b69801df4ea83cd3b5dbf0
Apple <farhad9>
2023-07-02 19:06:27 +0500

    signout functionality done

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,6 +1,9 @@
+"use client"
 import NewChat from "./NewChat"
+import { useSession, signOut } from "next-auth/react"
 
 function Sidebar() {
+  const {data: session} = useSession();
   return (
     <div className="flex flex-col 2x h-screen">
         <div className="flex-1">
@@ -12,6 +15,12 @@ function Sidebar() {
                 {/* chat list */}
             </div>
         </div>
+        {session && (<img 
+          src={session.user?.image!} 
+          className="w-12 h-12 rounded-full mx-auto mb-2 hover:opacity-50 cursor-pointer"
+          alt="profile"
+          onClick={() => signOut()} 
+           />)}
     </div>
   )
 }
